ef867236b078a34785f0cfe2b0810f1331ec17ca
TahaMerzouki <tmerzouki@insea.ac.ma>
2024-09-23 16:45:51 +0100

    chore: Add config in gitignore

--- src/includes/config.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-raw_folder_path = "/mnt/waterprojectdl/raw"
-processed_folder_path = "/mnt/waterprojectdl/processed"
-presentation_folder_path ="/mnt/waterprojectdl/presentation"
